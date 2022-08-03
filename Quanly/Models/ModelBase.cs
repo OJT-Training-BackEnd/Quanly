f5c3a583d26e1a4d4fd7102b79a590e5eee542b0
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2022-08-03 09:00:45 +0700

    Dat - CustomerSeedingData && Fix Some properties

--- Quanly/Models/ModelBase.cs
+++ Quanly/Models/ModelBase.cs
@@ -6,6 +6,6 @@
         public string Updater { get; set; } = string.Empty;
         public string Note { get; set; } = string.Empty;
         public DateTime DateAdded { get; set; } = DateTime.Now;
-        public DateTime DateUpdated { get; set; }
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
     }
 }
