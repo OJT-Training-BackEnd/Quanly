dc01ed91b37a56b7217d73f046142a055b15a347
Phuong Nguyen <85018054+nguyenlamthuyphuong25@users.noreply.github.com>
2022-08-03 11:23:46 +0700

    1st add FE code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import Login from '../src/pages/login/Login'
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import "./Login.scss";
+import { Button, Checkbox, Form, Input } from 'antd';
+
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+function Login() {
+  return (
+    <>
+      <div id="login">
+        <div id="loginLeft" flex={3}>
+          <div id="loginLeftInsideOut">
+            <div id="loginLeftInside">
+              <img src="https://images.unsplash.com/photo-1628891439478-c613e85af7d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+            </div>
+          </div>
+        </div>
+        <div id="loginRight" flex={2}>
+          <div id="loginRightInsideOut">
+            <h2>LOG IN ACCOUNT</h2>
+            <Form
+              name="basic"
+              labelCol={{
+                span: 8,
+              }}
+              wrapperCol={{
+                span: 16,
+              }}
+              initialValues={{
+                remember: true,
+              }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                ]}
+              >
+                <Input placeholder="input username"/>
+              </Form.Item>
+
+              <Form.Item
+                label="Password"
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password!",
+                  },
+                ]}
+              >
+                <Input.Password placeholder="input password" />
+              </Form.Item>
+
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <Form.Item
+                wrapperCol={{
+                  offset: 8,
+                  span: 16,
+                }}
+              >
+                <Button type="primary" htmlType="submit">
+                  Login
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
