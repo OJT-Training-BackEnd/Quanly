f9a5c410bd9164cdf89b44033b3abee3a42b8aec
ManhCode-TuongMinh <phanduchuy812@gmail.com>
2022-08-04 18:45:38 +0700

    Manh - Display Accumulate point & Delete Accumulate point

---
+++ Quanly/ValidationHandling/AccumulatePointsValidation/DeleteAccumulatePoints.cs
@@ -0,0 +1,25 @@
+﻿using Quanly.Data;
+
+namespace Quanly.ValidationHandling.AccumulatePointsValidation
+{
+    public class DeleteAccumulatePoints
+    {
+        private readonly DataContext _dataContext;
+
+        public DeleteAccumulatePoints(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public string DeletePoints (int id)
+        {
+            var validId = _dataContext.AccumulatePoints.FirstOrDefault(x => x.Id == id);
+
+             if(validId == null )
+            {
+                return "This customer does not existed";
+            }
+            return "ok";
+        }
+
+    }
+}
---
+++ Quanly/ValidationHandling/AccumulatePointsValidation/ValidGetAllAccumulatePoints.cs
@@ -0,0 +1,16 @@
+﻿using Quanly.Models.AccumulatePoints;
+
+namespace Quanly.ValidationHandling.AccumulatePointsValidation
+{
+    public class ValidGetAllAccumulatePoints
+    {
+        public string AccumulatePoints(List<AccumulatePoint> points)
+        {
+            if(points.Count == 0)
+                return "This customer have no point left";
+            
+            return "ok";
+            
+        }
+    }
+}
