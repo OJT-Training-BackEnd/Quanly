03d146fd0e40f4b14f4f29fbc6713b8120fb2988
Tran Thanh Dat <97006331+DatTranLK@users.noreply.github.com>
2022-08-05 17:32:15 +0700

    Dat - Done Create new Accumulate Point

---
+++ Quanly/Migrations/20220805101155_NullableToAccumulatePointRule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quanly.Migrations
+{
+    public partial class NullableToAccumulatePointRule : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccumulatePoints_AccumulatePointsRules_AccumulatePointsRulesId",
+                table: "AccumulatePoints");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccumulatePointsRulesId",
+                table: "AccumulatePoints",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "BirthDate", "DateOfRecord", "IssueDate" },
+                values: new object[] { new DateTime(2022, 8, 5, 17, 11, 54, 850, DateTimeKind.Local).AddTicks(672), new DateTime(2022, 8, 5, 17, 11, 54, 850, DateTimeKind.Local).AddTicks(688), new DateTime(2022, 8, 5, 17, 11, 54, 850, DateTimeKind.Local).AddTicks(685) });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccumulatePoints_AccumulatePointsRules_AccumulatePointsRulesId",
+                table: "AccumulatePoints",
+                column: "AccumulatePointsRulesId",
+                principalTable: "AccumulatePointsRules",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccumulatePoints_AccumulatePointsRules_AccumulatePointsRulesId",
+                table: "AccumulatePoints");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AccumulatePointsRulesId",
+                table: "AccumulatePoints",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "BirthDate", "DateOfRecord", "IssueDate" },
+                values: new object[] { new DateTime(2022, 8, 5, 8, 47, 32, 107, DateTimeKind.Local).AddTicks(4877), new DateTime(2022, 8, 5, 8, 47, 32, 107, DateTimeKind.Local).AddTicks(4892), new DateTime(2022, 8, 5, 8, 47, 32, 107, DateTimeKind.Local).AddTicks(4889) });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccumulatePoints_AccumulatePointsRules_AccumulatePointsRulesId",
+                table: "AccumulatePoints",
+                column: "AccumulatePointsRulesId",
+                principalTable: "AccumulatePointsRules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Quanly/Services/AccumulatePointsService/IAccumulatePointsService.cs
+++ Quanly/Services/AccumulatePointsService/IAccumulatePointsService.cs
@@ -1,4 +1,5 @@
 ﻿using Quanly.Models.AccumulatePoints;
+using Quanly.Models.Customers;
 using Quanly.Models.MemberCards;
 
 namespace Quanly.Services.ValidPointsService
@@ -6,11 +7,10 @@ namespace Quanly.Services.ValidPointsService
     public interface IAccumulatePointsService
     {
         Task<ServiceResponse<List<AccumulatePoint>>> GetAllAccumulatePoints();
-
         Task<ServiceResponse<List<AccumulatePoint>>> DeleteAccumulatePoints(int id);
-
         Task<ServiceResponse<AccumulatePoint>> UpdateAccumulatePoints(AccumulatePoint accumulatePoint, string cardNumber);
-
         Task<ServiceResponse<AccumulatePoint>> search(string cardNumber);
+        Task<ServiceResponse<AccumulatePoint>> CreateAccumulatePoint(AccumulatePoint accumulatePoint);
+/*        Task<ServiceResponse<double>> CalculatePoint(string type, string cardNumber, double money, double newPoint);  */  
     }
 }
