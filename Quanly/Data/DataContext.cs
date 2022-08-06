8e2ba71e72444812c3b65a167a0e004c6419e8fe
ninehnineh <103179810+ninehnineh@users.noreply.github.com>
2022-08-06 14:24:18 +0700

    Chinh - Delete seeding data

--- Quanly/Data/DataContext.cs
+++ Quanly/Data/DataContext.cs
@@ -15,38 +15,6 @@ namespace Quanly.Data
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Customer>()
-                .HasData(
-                    new Customer
-                    {
-                        Id = 1,
-                        Code = "KH123456789",
-                        CustomerName = "Cong Chinh",
-                        Address = "District 9, Ho Chi Minh City",
-                        Type = "Silver",
-                        Phone = "0123456789",
-                        TelePhone = "01234567891",
-                        Email = "Chinhpro@gmail.com",
-                        Fax = "+84 (8) 3823 3318",
-                        Gender = "Male",
-                        BirthDate = DateTime.Now,
-                        IdentityCard = "343456771234",
-                        IssueDate = DateTime.Now,
-                        CompanyName = "KNS",
-                        CompanyPhone = "01234567891",
-                        Contact = "An Ngo",
-                        Position = "Head of KNS",
-                        District = "District 9",
-                        Language = "Vietnamese",
-                        Age = "20",
-                        DateOfRecord = DateTime.Now,
-                        Importer = "Ad",
-                    }
-                );
-        }
-
         public DbSet<User> User { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<MemberCard> MemberCards { get; set; }
